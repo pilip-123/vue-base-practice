3659383f330f85420aa8a4ea2e50219a86623b83
pilip-123 <pilipyom985@gmail.com>
2026-06-16 15:50:39 +0700

    main code

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+<template>
   <div class="min-h-screen bg-gradient-to-b from-gray-50 to-white">
     <div class="mx-auto max-w-6xl px-4 py-8">
       <header class="mb-6">
@@ -27,9 +27,9 @@
       />
     </div>
   </div>
-</template> -->
+</template>
 
-<!-- <script setup>
+<script setup>
 import { computed, ref } from "vue";
 import StudentList from "./components/StudentList.vue";
 import StudentDetail from "./components/StudentDetail.vue";
@@ -106,81 +106,5 @@ const deleteStudent = (id) => {
     selectedStudent.value = null;
   }
 };
-</script> -->
-
-<template>
-<div class="container w-1/2 h-32 mt-4 flex gap-4 m-auto">
-   <!-- product 1  -->
-  <ProductCard :isLiked="true">
-  <template #badge>
-<span class="bg-green-500 text-white px-2 py-1 rounded">New</span>
-  </template>
-
-  <template #default="{liked}">
-    <h2 class="text-xl font-bold">iPhone 17</h2>
-    <p>Latest Apple smartphone.</p>
-  <span v-if="liked">hart</span>
-  </template>
-
-  <template #actions>
-    <button class="bg-blue-500 text-white px-3 py-1 rounded">Add to card</button>
-    <button class="bg-gray-500 text-white px-3 py-1 rounded">Whishlist</button>
-  </template>
-
-  </ProductCard>
-
-  <!-- Product 2  -->
-   <ProductCard :isLiked="false">
-    <template #badge>
-      <span class="bg-red-500 text-white px-2 py-1 rounded">Sale</span>
-    </template>
-
-    <template #default="{liked}">
-      <h2 class="text-xl font-bold">MacBook Pro</h2>
-      <p>Powerful laptop for developer.</p>
-      <span v-if="liked">Hard</span>
-    </template>
-
-    <template #actions>
-    <button class="bg-blue-500 text-white px-3 py-1 rounded">Add to card</button>
-    <button class="bg-gray-500 text-white px-3 py-1 rounded">Whishlist</button>
-  </template>
-
-   </ProductCard>
-
-     <!-- Product 3 -->
-  <ProductCard :isLiked="true">
-
-    <template #badge>
-      <span class="bg-purple-500 text-white px-2 py-1 rounded">
-        HOT
-      </span>
-    </template>
-
-    <template #default="{ liked }">
-      <h2 class="text-xl font-bold">
-        AirPods Pro
-      </h2>
-
-      <p>Wireless earbuds with noise cancellation.</p>
-
-      <span v-if="liked">Hard</span>
-    </template>
-
-    <template #actions>
-      <button class="bg-blue-500 text-white px-3 py-1 rounded">
-        Add to Cart
-      </button>
-
-      <button class="bg-gray-500 text-white px-3 py-1 rounded">
-        Wishlist
-      </button>
-    </template>
-
-  </ProductCard>
-</div>
-</template>
-
-<script setup>
-import ProductCard from './components/Practices/ProductCard.vue';
 </script>
+
